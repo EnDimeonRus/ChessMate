dc7272d25aec442530f7a558fbb8c19a58cb1e63
Dmitry_Kotov <endimeon@mail.ru>
2022-06-05 01:10:28 +0400

    Added initial project with DB initialization

---
+++ ChessMate.Infrastructure/ChessMateDbContext.cs
@@ -0,0 +1,17 @@
+﻿using ChessMate.Infrastructure.Models;
+using System.Data.Entity;
+
+namespace ChessMate.Infrastructure
+{
+    public class ChessMateDbContext : DbContext
+    {
+        public ChessMateDbContext() : base("Data Source=EPRUSAMW00DB\\SQLEXPRESS;Initial Catalog=ChessMate;Integrated Security=True")
+        {
+            Database.SetInitializer(new ChessMateInitializer());
+        }
+        
+        public DbSet<Color> Colors { get; set; }
+        public DbSet<Figure> Figures { get; set; }
+        public DbSet<Position> Positions { get; set; }
+    }
+}
---
+++ ChessMate.Infrastructure/ChessMateInitializer.cs
@@ -0,0 +1,28 @@
+﻿using ChessMate.Infrastructure.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessMate.Infrastructure
+{
+    public class ChessMateInitializer : DropCreateDatabaseIfModelChanges<ChessMateDbContext>
+    {
+        protected override void Seed(ChessMateDbContext context)
+        {
+            context.Colors.Add(new Color { Description = "Белый" });
+            context.Colors.Add(new Color { Description = "Черный" });
+
+            context.Figures.Add(new Figure() { Code = "п", Description = "Пешка" });
+            context.Figures.Add(new Figure() { Code = "С", Description = "Слон" });
+            context.Figures.Add(new Figure() { Code = "К", Description = "Конь" });
+            context.Figures.Add(new Figure() { Code = "Л", Description = "Ладья" });
+            context.Figures.Add(new Figure() { Code = "Ф", Description = "Ферзь" });
+            context.Figures.Add(new Figure() { Code = "Кр", Description = "Король" });
+
+            base.Seed(context);
+        }
+    }
+}

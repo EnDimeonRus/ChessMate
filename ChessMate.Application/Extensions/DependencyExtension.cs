d10b6e0280721515487820d3ae6bf5e8b99c693b
Dmitry_Kotov <endimeon@mail.ru>
2022-06-05 14:12:46 +0400

    Added PositionManager class Added UnitTest project

---
+++ ChessMate.Application/Extensions/DependencyExtension.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessMate.Application.Extensions
+{
+    public static class DependencyExtension
+    {
+        public static void RegisterManagers(this IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddTransient<IPositionManager, PositionManager>();
+        }
+    }
+}
---
+++ ChessMate.Application/IPositionManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessMate.Application
+{
+    public interface IPositionManager
+    {
+        void SetPosition(int figure, int color, string oldPosition, string newPosition);
+
+    }
+}
